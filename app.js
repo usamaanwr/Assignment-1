a128a8822619c197e7726e28b5980bf5c42c89e8
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2024-01-24 09:47:03 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Asignemnet
+
+var age ="22 years old"
+var isstudent ="yes i,m intermediate student"
+console.log(age);
+console.log(isstudent);
+alert("Asalam o Alaikum.My name is Muhammad osama i,m frontend developer ")
+
+// const h11 = document.getElementById('my')
+// function textcenter(){
+//     const h11 = document.getElementById('textcolour').style.textAlign ='center'
+//     }
+// function textcenter(){
+//     const h11 = document.getElementById('textcolour').style.backgroundColor ='red'
+// }
